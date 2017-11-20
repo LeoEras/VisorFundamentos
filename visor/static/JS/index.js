2484eae76ed9abc22b88a550fb2e3e56a3004681
Leonardo Eras <leonardo.eras@cti.espol.edu.ec>
2017-11-20 14:05:36 -0500

    Ahora muestra en rojo la linea con error

--- visor/static/JS/index.js
+++ visor/static/JS/index.js
@@ -11,11 +11,83 @@ function AttachID(){
     }
 }
 
+function getCookie(name) {
+       var cookieValue = null;
+       if (document.cookie && document.cookie != '') {
+         var cookies = document.cookie.split(';');
+         for (var i = 0; i < cookies.length; i++) {
+         var cookie = jQuery.trim(cookies[i]);
+         // Does this cookie string begin with the name we want?
+         if (cookie.substring(0, name.length + 1) == (name + '=')) {
+             cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+             break;
+          }
+     }
+ }
+ return cookieValue;
+}
+
 function ClickEvent(event){
+    event.preventDefault();
     AttachID();
-    var list_id = $(".identificadores");
-    for(var i = 0; i < list_id.length; i++){
-        list_id[i] = list_id[i].innerText;
+    var csrftoken = getCookie('csrftoken');
+    var item_id = event.target.id;
+    var allrows = $(".rows");
+    var allitems = [];
+    for (var i = 0; i < allrows.length; i++) {
+        allitems[i] = allrows[i].id;
     }
-    alert(event.target.id);
+    var index = allitems.indexOf(item_id);
+    var allerrors = $(".errors");
+    var line_error = allerrors[index];
+    //alert();
+    $.ajax({
+         url : window.location.href, // the endpoint,commonly same url
+         type : "POST", // http method
+         data : { csrfmiddlewaretoken : csrftoken,
+         id : item_id,
+    }, // data sent with the post request
+    success : function(json) {
+        var file_text = json.file_open;
+        file_text = file_text.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        file_text = file_text.replace(/(?:\r\t|\r|\t)/g, '&emsp;&emsp;');
+        document.getElementById("textarea").innerHTML = file_text;
+        //document.getElementById("textarea").innerHTML = file_text;
+      //console.log(json); // another sanity check
+      //On success show the data posted to server as a message
+      //alert('Hi '+json['email'] +'!.' + ' You have entered password:'+      json['password']);
+        //console.log($("#textarea"));
+        var allchildren = document.getElementById("textarea").childNodes;
+        var linesOnly = []
+        var prev_tag;
+
+        for (var i = 0; i < allchildren.length; i++) {
+            if(allchildren[i].tagName != 'BR'){
+                var newP = document.createElement("p");
+                newP.innerHTML = allchildren[i].data;
+                document.getElementById("textarea").replaceChild(newP, allchildren[i]);
+            }
+        }
+
+        allchildren = document.getElementById("textarea").childNodes;
+        for (var i = 0; i < allchildren.length; i++) {
+            if (i > 0){
+                prev_tag = allchildren[i - 1];
+            }
+
+            if((allchildren[i].tagName == 'BR' && prev_tag.tagName != 'BR') || (allchildren[i].tagName == 'BR' && prev_tag.tagName == 'BR')){
+                linesOnly.push(allchildren[i - 1]);
+            }
+        }
+        linesOnly.push(allchildren.pop);
+
+        linesOnly[parseInt(line_error.innerText) - 1].style.color = 'red';
+        //document.getElementById("textarea").replaceChild(newP, linesOnly[parseInt(line_error.innerText) - 1]);
+
+     },
+
+     // handle a non-successful response
+     error : function(xhr,errmsg,err) {
+     console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+     }});
 }
--- visor/views.py
+++ visor/views.py
@@ -1,22 +1,71 @@
 from django.shortcuts import render
+import re
 from os import path
+import glob, os
 import xml.etree.ElementTree
+import json
+from django.http import JsonResponse
+from django.template import RequestContext
 #from django.http import HttpResponse
+
+listErrorPython = ['StandardError', 'ArithmeticError', 'BufferError', 'LookupError', 'EnvironmentError',
+					   'AssertionError', 'AttributeError', 'EOFError',
+					   'FloatingPointError', 'GeneratorExit', 'IOError', 'ImportError', 'IndexError', 'KeyError',
+					   'KeyboardInterrupt', 'MemoryError',
+					   'NameError', 'NotImplementedError', 'OSError', 'OverflowError', 'ReferenceError', 'RuntimeError',
+					   'StopIteration', 'SyntaxError',
+					   'IndentationError', 'TabError', 'SystemError', 'SystemExit', 'TypeError', 'UnboundLocalError',
+					   'UnicodeError', 'UnicodeEncodeError',
+					   'UnicodeDecodeError', 'UnicodeTranslateError', 'ValueError', 'VMSError', 'WindowsError',
+					   'ZeroDivisionError', 'FileNotFoundError']
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        if request.is_ajax():
+            id_file = request.POST.get('id')
+            path_file = path.realpath("visor\\gits\\2taller\\")
+            file_open = open("".join(glob.iglob(os.path.join(path_file, "*" + id_file + "*"))), encoding='utf-8')
+            file_open = file_open.read()
+            #return JsonResponse({'file_open': json.dumps(file_open.decode("utf-8"))})
+            return JsonResponse({'file_open': file_open})
     logs = path.realpath("visor\\gits\\2taller\\log.xml")
     e = xml.etree.ElementTree.parse(logs).getroot()
     list_output = []
     output_id = []
+    error_caller_file = []
+    line_error = []
     for atype in e.findall('log_project'):
+        list_of_possible_error_files = []
+        list_of_possible_line_error = []
+        name_proy = atype.get('name_project')
         if atype.text is not None:
             for line in atype.text.splitlines():
-                if "Error" in line:
-                    list_output.append(line)
-                    output_id.append(atype.get('id'))
-    #file = open(logs)
-    #content = file.read()
+                for item in listErrorPython:
+                    if item in line:
+                        list_output.append(line)
+                        output_id.append(atype.get('id'))
+
+            for line in atype.text.splitlines():
+                if "File" in line and "line" in line and ".py" in line and name_proy in line: #El nombre del proyecto no debe de ir en blanco
+                    line = line.replace('"', "")
+                    line = str.split(line, "/")
+                    for item in line:
+                        if ".py" in item:
+                            item = item.split(",")
+                            list_of_possible_error_files.append(item[0])
+                            list_of_possible_line_error.append(int(re.search(r'\d+', item[1]).group()))
+
+
+            if len(list_of_possible_error_files) > 0:
+                error_caller_file.append(list_of_possible_error_files[-1])
+                line_error.append(list_of_possible_line_error[-1])
+
+    for i in range(len(list_output)):
+        output_id[i] = output_id[i] + "_" + error_caller_file[i]
+
     context = {"output_list": list_output,
-               "output_id": output_id}
+               "output_id": output_id,
+               "line_errors": line_error}
     return render(request, 'visor/index.html', context)
+    #return render_to_response('visor/index.html', context_instance=RequestContext(request))
